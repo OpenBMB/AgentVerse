e4876d737d8e6f62bb73cd7fe050f00e9c09d08e
Yusheng Su <ky200120000@gmail.com>
2023-05-09 00:20:31 +0800

    update readme

--- main_demo.py
+++ main_demo.py
@@ -1,4 +1,22 @@
 from agentverse.demo import UI
+from argparse import ArgumentParser
 
-ui = UI('nlp_classroom_9players')
+
+parser = ArgumentParser()
+parser.add_argument("--task", type=str, default="nlp_classroom_9players")
+args = parser.parse_args()
+
+#available
+#args.task = "nlp_classroom_9players"
+#args.task = "nlp_classroom_9players_group"
+
+
+'''
+#[to-do] tool unavailable
+#args.task = "math_problem_2players_tools"
+#args.task = "nlp_classroom_3players_withtool"
+#args.task = "nlp_classroom_3players"
+'''
+
+ui = UI(args.task)
 ui.launch()
