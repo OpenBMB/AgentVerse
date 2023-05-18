7d2eeeb1c6e6ddfbe43f51b293994e2e8eb2e3ee
Chan Chi-Min <75533759+chanchimin@users.noreply.github.com>
2023-05-18 14:06:03 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from agentverse.agentverse import AgentVerse
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument("--task", type=str, default="prisoner_dilema")
-args = parser.parse_args()
-
-agentverse = AgentVerse.from_task(args.task)
-agentverse.run()
