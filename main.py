38541500e97545d4d4246867c4285dbe211acdfa
Yusheng Su <ky200120000@gmail.com>
2023-05-28 17:51:13 +0800

    update requirements.txt and test coding case

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from agentverse.agentverse import AgentVerse
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("--task", type=str, default="nlp_classroom_3players")
+#parser.add_argument("--task", type=str, default="nlp_classroom_3players")
+#parser.add_argument("--task", type=str, default="sde_team")
+parser.add_argument("--task", type=str, default="sde_team/sde_team_2players_nolc")
 
 args = parser.parse_args()
 agentverse = AgentVerse.from_task(args.task)
--- main_demo.py
+++ main_demo.py
@@ -4,7 +4,8 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 # parser.add_argument("--task", type=str, default="nlp_classroom_9players")
 # parser.add_argument("--task", type=str, default="nlp_classroom_3players_nolc")
-parser.add_argument("--task", type=str, default="prisoner_dilema")
+#parser.add_argument("--task", type=str, default="prisoner_dilema")
+parser.add_argument("--task", type=str, default="sde_team/sde_team_2players_nolc")
 args = parser.parse_args()
 
 # available
