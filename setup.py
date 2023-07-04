24a125fdebe09f3e537bad3259c272fb14ce67ba
chenweize1998 <chenweize1998@gmail.com>
2023-07-04 10:15:18 +0800

    package release

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ import subprocess
 # with open("requirements.txt", "r") as f:
 #     requirements = f.read().splitlines()
 
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="agentverse",
-    version="0.1.0",
+    version="0.1.3",
     author="OpenBMB",
     author_email="chenweize1998@gmail.com",
     description="A versatile framework that streamlines the process of creating custom multi-agent environments for large language models (LLMs).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/OpenBMB/AgentVerse",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -32,4 +37,5 @@ setuptools.setup(
         "astunparse",
         "langchain",
     ],
+    include_package_data = True,
 )
