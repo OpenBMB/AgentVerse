cb5772f8b2157ffdf1d246567ab4d050e82d2ed2
chenweize1998 <chenweize1998@gmail.com>
2023-07-06 08:56:33 +0800

    fix importing bugs introduced in memory manipulator

--- agentverse/memory_manipulator/__init__.py
+++ agentverse/memory_manipulator/__init__.py
@@ -3,5 +3,6 @@ from agentverse.registry import Registry
 memory_manipulator_registry = Registry(name="Memory_Manipulator_Registry")
 
 from .base import BaseMemoryManipulator
+from .basic import BasicMemoryManipulator
 from .reflection import Reflection
 
--- agentverse/memory_manipulator/basic.py
+++ agentverse/memory_manipulator/basic.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, Field
 
 from agentverse.message import Message
 from agentverse.memory_manipulator import BaseMemoryManipulator
+from . import memory_manipulator_registry
 
 @memory_manipulator_registry.register("basic")
 class BasicMemoryManipulator(BaseMemoryManipulator):
