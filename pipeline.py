0381fe09dafd11bbbf4a1e0c1d5e282a1eed2a9e
chenweize1998 <chenweize1998@gmail.com>
2023-09-14 13:21:28 +0800

    minor fix

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,18 @@
+import logging
+
 # from agentverse.agentverse import AgentVerse
 from agentverse.agentversepipeline import AgentVersePipeline
 from agentverse.demo import UI
+from agentverse.logging import logger
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
 # case: pipeline
 # parser.add_argument("--task", type=str, default="pipeline")
-parser.add_argument("--single_agent", "-s", action="store_true")
-parser.add_argument("--discussion_mode", "-d", action="store_true")
+parser.add_argument("--single_agent", "-s", action="store_true")  # Deprecated
+parser.add_argument("--discussion_mode", "-d", action="store_true")  # Deprecated
+parser.add_argument("--debug", action="store_true")
 
 # case: code writing: create a caculator
 # parser.add_argument("--task", type=str, default="pipeline_pythoncalculator")
@@ -21,6 +25,8 @@ parser.add_argument("--task", type=str, default="pipeline_brainstorming")
 
 
 args = parser.parse_args()
+
+logger.set_level(logging.DEBUG if args.debug else logging.INFO)
 
 # terminal
 agentversepipeline = AgentVersePipeline.from_task(args.task)
