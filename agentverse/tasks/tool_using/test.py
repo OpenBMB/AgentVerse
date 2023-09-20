931dad535e5e2b71a0064d70a9a53c910c822191
Heyang Yu <tsuruko_04@outlook.com>
2023-09-20 11:44:28 +0800

    Delete agentverse/tasks/tool_using/test.py

--- agentverse/tasks/tool_using/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-url  = "http://8.217.97.110:8080/get_available_tools"
-param = {
-    "tool_category": 
-        ["autogpt"]
-}
-response = requests.post(url, json=param, headers={"toolbench_key":"p5ZASSLBO0EknAQLE5ecNZ7kq5i1YfY9eoWUXNxL3TM6lXwdXs"}, timeout=10)
-result = response.json()
-print(result)
