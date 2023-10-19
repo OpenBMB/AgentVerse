36f2f2399b3c9c268c7f16405470135081d9e2a0
Weize Chen <32613237+chenweize1998@users.noreply.github.com>
2023-10-19 10:26:45 +0800

    remove personal info in run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,9 +1,11 @@
 :<<COMMENT
 See https://github.com/lm-sys/FastChat/blob/main/docs/openai_api.md for more usages.
 COMMENT
-export CUDA_VISIBLE_DEVICES=1
+# export CUDA_VISIBLE_DEVICES=0
+MODEL_PATH="path_to_the_downloaded_model_dir"
+MODEL_NAME="name_of_the_model"
 python3 -m fastchat.serve.controller & \
 python3 -m fastchat.serve.multi_model_worker \
-    --model-path /data/private/xiayu/huggingface_cache/Llama-2-7b-chat-hf \
-    --model-names llama-2-7b-chat-hf & \
+    --model-path ${MODEL_PATH} \
+    --model-names ${MODEL_NAME} & \
 python3 -m fastchat.serve.openai_api_server --host localhost --port 5000
