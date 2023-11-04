e1e54f6c8cbe82239959c702496df9f0e9be4352
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2023-11-04 16:47:26 +0800

    Update __init__.py

--- agentverse/agents/company_agent/__init__.py
+++ agentverse/agents/company_agent/__init__.py
@@ -1,6 +1,3 @@
-from .Company import Company
-from .Department import Department
 from .Role import Role
-from .AgentPool import AgentPool
 from .Planner import Planner
-from .Collaborator import Collaborator
+from .Recruiter import Recruiter
